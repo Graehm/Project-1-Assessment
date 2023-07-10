e341a2bf23a78768ba5100c4ab421bb9cbd156b8
Graehm <gpatrellfazio@gmail.com>
2023-07-10 18:31:24 -0500

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const CountEl = document.getElementById('count')
+const increaseEl = document.getElementById('increase')
+const displayValueEl = document.getElementById('displayValue')
+const decreaseEl = document.getElementById('decrease')
+
+
+// let counter = num(theCountEl.value)
+
+increaseEl.addEventListener('click', () => {
+
+})
+
+decreaseEl.addEventListener('click', () => {
+    
+})
